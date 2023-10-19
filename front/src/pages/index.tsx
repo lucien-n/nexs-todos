e30ee0777274475d675d38a219cb6411e706ab59
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-10-19 11:24:52 +0200

    feat: basic get todos query

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,5 +1,24 @@
+import { Todo as TTodo } from "@/__generated__/graphql";
+import { GET_TODOS } from "@/lib/gql/queries/todo";
+import { useQuery } from "@apollo/client";
+import { useEffect, useState } from "react";
+
 const Index = () => {
-  return <h1>hello</h1>;
+  const { data } = useQuery(GET_TODOS);
+  const [todos, setTodos] = useState<TTodo[]>([]);
+
+  useEffect(() => {
+    if (!data?.todos) return;
+    setTodos(data.todos);
+  }, [data]);
+
+  return (
+    <h1>
+      {todos.map((todo) => (
+        <div key={todo.id}>{todo.content}</div>
+      ))}
+    </h1>
+  );
 };
 
 export default Index;

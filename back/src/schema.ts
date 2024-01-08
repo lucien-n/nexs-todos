df8099dd196d7744fad3421bd3063afc3fcbae83
lucien-neuhoff <lucien.neuhoff@everping.eu>
2024-01-08 12:23:51 +0100

    fix: schema.ts

--- back/src/schema.ts
+++ back/src/schema.ts
@@ -3,7 +3,9 @@ import {
   GraphQLSchemaBuilderModule,
   GraphQLSchemaFactory,
 } from '@nestjs/graphql';
-import { Todo } from './todo/entities/todo.entity';
+import { TodoResolver } from './todo/todo.resolver';
+import { mkdir, writeFileSync } from 'fs';
+import { printSchema } from 'graphql';
 
 const generateSchema = async () => {
   const app = await NestFactory.create(GraphQLSchemaBuilderModule);
@@ -11,7 +13,10 @@ const generateSchema = async () => {
   await app.init();
 
   const gqlSchemaFactory = app.get(GraphQLSchemaFactory);
-  const schema = await gqlSchemaFactory.create([Todo]);
+  const schema = await gqlSchemaFactory.create([TodoResolver]);
+  mkdir('./__generated__', () => {
+    writeFileSync('./__generated__/schema.gql', printSchema(schema));
+  });
 };
 
 generateSchema();
